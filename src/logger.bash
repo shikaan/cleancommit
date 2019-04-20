577ed6949482f93a52700c0e0d2349cb5be2191a
shikaan <spagnolo.manu@gmail.com>
2019-04-20 11:32:25 +0200

    chore: move logger to a meaningful place

--- src/logger.bash
+++ src/logger.bash
@@ -14,7 +14,7 @@
 ##
 
 exec 3>&2 # logging stream (file descriptor 3) defaults to STDERR
-verbosity=3 # default to show warnings
+verbosity=1 # default to show warnings
 silent_lvl=0
 crt_lvl=1
 err_lvl=2
