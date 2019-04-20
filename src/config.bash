24038e2b8f51f1cd026f263a2d44d365ac022a9f
shikaan <spagnolo.manu@gmail.com>
2019-04-20 11:32:01 +0200

    feat: add configuration parser

---
+++ src/config.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+get_configuration_by_type_and_key_from_file() {
+    TYPE="$1"
+    KEY="$2"
+    FILE="$3"
+    
+    echo `sed -nr "/^\[$TYPE\]/ { :l /^$KEY[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $FILE`
+}
