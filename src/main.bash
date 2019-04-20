914ea44c92229fe5d06d5eeed39b381a70a9d899
shikaan <spagnolo.manu@gmail.com>
2019-04-20 11:34:36 +0200

    feat: init main

---
+++ src/main.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+import_by_relative_path() {
+    RELATIVE_PATH="$1"
+
+    DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+    . "$DIRNAME/$RELATIVE_PATH"
+}
+
+import_by_relative_path "title/_index.bash"
+
+CONFIG_FILE=$1
+MESSAGE=$2
+
+check_title_type_by_message_and_configuration "$MESSAGE" "$CONFIG_FILE"
--- src/title/_index.bash
+++ src/title/_index.bash
@@ -1,26 +1,56 @@
 #! /bin/bash
-DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-. "$DIRNAME/../utils.bash"
-. "$DIRNAME/type.bash"
+import_by_relative_path() {
+    RELATIVE_PATH="$1"
 
-ALLOWED_RULES=type_enum,type_empty,type_lowercase,type_uppercase
+    DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# TODO: replace with input from configuration
-INPUT_RULES=type_enum,type_empty
-MESSAGE='feat(1): test\nanother thing'
+    . "$DIRNAME/$RELATIVE_PATH"
+}
 
-check_message_type() {
+import_by_relative_path "../utils.bash"
+import_by_relative_path "../config.bash"
+import_by_relative_path "type.bash"
+
+import_by_relative_path "../logger.bash"
+
+ALLOWED_RULES=type_empty,type_lowercase,type_uppercase,type_enum
+
+check_title_type_by_message_and_configuration() {
     MESSAGE=$1
-    RULES=$2
+    CONFIGURATION_FILE=$2
 
-    rules_as_list=`get_list_from_comma_separated_string "$RULES"`
+    debug "check_title_type_by_message_and_configuration (MESSAGE=\"$MESSAGE\" CONFIGURATION_FILE=\"$CONFIGURATION_FILE\")"
+
+    rules_as_list=`get_list_from_comma_separated_string "$ALLOWED_RULES"`
+    get_list_from_comma_separated_string_exit_code=$?
+
+    debug "get_rules_as_list result $get_list_from_comma_separated_string_exit_code"
+
+    if [ $get_list_from_comma_separated_string_exit_code -eq 1 ]
+    then
+        throw "Unable to parse list of allowed rules. Received error: \"$rules_as_list\""
+    fi
+
     type=`get_type_from_message "$MESSAGE"`
+    get_type_from_message_exit_code=$?
+
+    debug "get_type_from_message $get_type_from_message_exit_code"
+    
+    if [ $get_type_from_message_exit_code -eq 1 ]
+    then
+        throw "Unable to parse commit message. Received error: \"$type\""
+    fi
 
     for rule in $rules_as_list
     do
-        "check_$rule" "$type" "feat,fix,test"
+        rule_argument=`get_configuration_by_type_and_key_from_file "Title" "$rule" "$CONFIGURATION_FILE"`   
+
+        if [ ! -z $rule_argument ] && [ $rule_argument != "0" ]
+        then
+            debug "Found rule $rule with $rule_argument"
+            "check_$rule" "$type" "$rule_argument"
+            debug "Result $?"
+        fi
     done
 }
-
-check_message_type "$MESSAGE" "$INPUT_RULES" 
